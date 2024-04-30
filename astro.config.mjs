1ff167586e86204821513e325ab1e949d63ab30d
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-04-30 23:26:35 +0900

    ✨ GitHub Pagesにデプロイするための変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), tailwind()]
+  integrations: [alpinejs(), tailwind()],
+  site: 'https://dampuzakura.github.io',
+  base: '/',
 });
