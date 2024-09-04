5fe019ed6c04d306ee5076b5c97009da255b85fe
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-09-05 00:42:18 +0900

    ✨ ブログのRSSフィードのタイトルと説明を変更

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,8 +5,8 @@ import { getPosts } from '../lib/posts.astro';
 export const GET: APIRoute = async (context) => {
   let posts = await getPosts();
   return rss({
-    title: 'Tovuwata Blog',
-    description: 'Tovuwata BlogのRSSフィードです。',
+    title: '段風桜のブログ',
+    description: '段風桜のブログのRSSフィードです。',
     site: context.site ?? '',
     items: posts.slice(0, 30).map((post) => ({
       title: post.frontmatter.title,
